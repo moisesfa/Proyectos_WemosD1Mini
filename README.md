e65dc1ef3403ad8537e743529549a0a9a1a3ce0e
moisesfa <moifdez@gmail.com>
2020-05-15 21:12:01 +0200

    Añado telegram_01

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Proyectos_WemosD1Mini
 Repositorio para proyectos con la placa Wemos D1 Mini 
 
-## deep_sleep
+## [deep_sleep](https://github.com/moisesfa/Proyectos_WemosD1Mini/blob/master/deep_sleep/src/main.cpp)
 Ejemplo básico de Deep Sleep - Sueño profundo con un WeMos D1 Mini
 
-## dht22_01
+## [dht22_01](https://github.com/moisesfa/Proyectos_WemosD1Mini/blob/master/dht22_01/src/main.cpp)
 Ejemplo básico para leer la Shield DHT22 Sensor de temperatura y humedad
 
-## dht22_02
+## [dht22_02](https://github.com/moisesfa/Proyectos_WemosD1Mini/blob/master/dht22_02/src/main.cpp)
 Ejemplo básico para leer la Shield DHT22 Sensor de temperatura y humedad, usando la función millis() en lugar de delay()
 
-## dht22_03
+## [dht22_03](https://github.com/moisesfa/Proyectos_WemosD1Mini/blob/master/dht22_03/src/main.cpp)
 Ejemplo básico para leer la Shield DHT22 Sensor de temperatura y humedad, después de la lectura entramos en Deep Sleep para ahorro energético
+
+## [telegram_01](https://github.com/moisesfa/Proyectos_WemosD1Mini/blob/master/telegram_01/src/main.cpp)
+Ejemplo básico de control del led integrado en la WeMos D1 Mini desde un bot de telegram.
+
---
+++ telegram_01/src/main.cpp
@@ -0,0 +1,136 @@
+/**
+ * Ejemplo básico de control del led integrado en la WeMos D1 Mini 
+ * desde un bot de telegram.
+ * 
+ * Fuente de información
+ * Control an LED strip using Inline Keyboard on Telegram
+    By Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Tindie: https://www.tindie.com/stores/brianlough/
+    Twitter: https://twitter.com/witnessmenow
+ * */
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+#include <UniversalTelegramBot.h>
+// Library for interacting with the Telegram API
+// Search for "Telegegram" in the Library manager and install
+// The universal Telegram library
+// https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+
+
+#include <ArduinoJson.h>
+// Library used for parsing Json from the API responses
+// NOTE: There is a breaking change in the 6.x.x version,
+// install the 5.x.x version instead
+// Search for "Arduino Json" in the Arduino Library manager
+// https://github.com/bblanchon/ArduinoJson
+
+//------- Replace the following! ------
+
+char ssid[] = "nombre de la red";         		// el nombbre de su red SSID
+char password[] = "contraseña de la red";       // la contraseña de su red
+
+#define TELEGRAM_BOT_TOKEN "token del bot de telegram"
+
+//------- ---------------------- ------
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(TELEGRAM_BOT_TOKEN, client);
+
+int Bot_mtbs = 1000; // tiempo medio entre escaneo de mensajes
+long Bot_lasttime;   // la última vez que se realizó la exploración de mensajes
+bool Start = false;
+// Estado del led
+int ledStatus = 0;
+
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i=0; i<numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    String text = bot.messages[i].text;
+
+    String from_name = bot.messages[i].from_name;
+    if (from_name == "") from_name = "Guest";
+
+    if (text == "/ledon") {
+      digitalWrite(LED_BUILTIN, LOW);   // LED ON
+      ledStatus = 1;
+      bot.sendMessage(chat_id, "Led is ON", "");
+    }
+
+    if (text == "/ledoff") {
+      ledStatus = 0;
+      digitalWrite(LED_BUILTIN, HIGH);    // LED OFF
+      bot.sendMessage(chat_id, "Led is OFF", "");
+    }
+
+    if (text == "/status") {
+      if(ledStatus){
+        bot.sendMessage(chat_id, "Led is ON", "");
+      } else {
+        bot.sendMessage(chat_id, "Led is OFF", "");
+      }
+    }
+
+    if (text == "/start") {
+      String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
+      welcome += "This is Flash Led Bot example.\n\n";
+      welcome += "/ledon : to switch the Led ON\n";
+      welcome += "/ledoff : to switch the Led OFF\n";
+      welcome += "/status : Returns current status of LED\n";
+      bot.sendMessage(chat_id, welcome, "Markdown");
+    }
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Establezca WiFi en modo estación y desconéctese de un AP si era anteriormente estaba conectado
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // Intente conectarse a la red Wifi
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(10);
+  digitalWrite(LED_BUILTIN, HIGH); // el led OFF al inicio
+
+  client.setInsecure();
+
+}
+
+void loop() {
+  if (millis() > Bot_lasttime + Bot_mtbs)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    Bot_lasttime = millis();
+  }
+}
